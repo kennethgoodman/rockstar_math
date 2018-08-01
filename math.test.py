a6e9fcb41a8b6cf846f3a1563c033d9016db2e3a
kennethgoodman <kennethsgoodman@yahoo.com>
2018-08-01 01:23:10 -0400

    adding test file

---
+++ math.test.py
@@ -0,0 +1,73 @@
+from rockmath import Factorial as rockfactorial, Power, LN as rockln, Square_Root as rocksqrt, Absolute_Value, the_e, Mod, LOG as rocklog, Exp
+from math import factorial as pyfactorial, log as pyln, sqrt as pysqrt
+import math
+
+def run_factorial_test():
+    for i in range(25):
+        print("testing factorial({})".format(i))
+        assert rockfactorial(i) == pyfactorial(i)
+    assert rockfactorial(-1) == -1
+
+def close(x,y):
+    return abs(x - y) < 1e-10 
+
+def assert_close(x, y, m=""):
+    if close(x,y):
+        assert True
+    else:
+        assert False, "py({}) is not close to rock({})-{}".format(x,y,m)
+
+def run_power_test():
+    for x in range(10):
+        for n in range(10):
+            print("testing {}**{}".format(x,n))
+            assert_close(x**n, Power(x,n), "Failed for {}**{}".format(x,n))
+
+def run_sqrt_test():
+    for x in range(1, 100):
+        print("testing sqrt({})".format(x))
+        assert_close(pysqrt(x), rocksqrt(x), "Failed for sqrt({})".format(x))
+
+def run_ln_test():
+    for x in range(1, 50):
+        print("testing ln({})".format(x))
+        assert_close(pyln(x), rockln(x), "Failed for ln({})".format(x)) 
+
+def run_abs_test():
+    for x in [-1, -40, -3.3, 0, 3, 32, 2]:
+        print("testing abs({})".format(x))
+        assert_close(abs(x), Absolute_Value(x), "Failed for abs({})".format(x))
+
+def run_e_test():
+    assert_closer(math.e, the_e, "Approximation of e")
+
+def run_log_test():
+    for x in range(1, 10):
+        for b in range(2, 5):
+            print("testing log_{}({})".format(b,x))
+            assert_close(pyln(x,b), rocklog(x, b), "Failed for log_{}({})".format(b,x))
+
+
+def run_exponential_test():
+    for x in range(10):
+        print("testing e ^ {}".format(x))
+        assert_close( math.e ** x, Exp(x), "Failed for e ^ {}".format(x))
+
+def run_mod_test():
+    for x in range(-25, 25):
+        for m in range(1, 10):
+            print("testing {} % {}".format(x,m))
+            assert_close(x % m, Mod(x,m), "Failed for {} % {}".format(x,m))
+
+def main():
+    run_factorial_test()
+    run_power_test()
+    run_ln_test()
+    run_sqrt_test()
+    run_abs_test()
+    run_log_test()
+    run_exponential_test()
+    run_mod_test()
+
+if __name__ == '__main__':
+    main()
